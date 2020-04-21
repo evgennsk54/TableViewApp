83d7b1832013e5b95ac53a0df04a9d3bc20f9e0f
Evgen <Evgennsk54@gmail.com>
2020-04-21 20:35:29 +0700

    Custom Table Cell

---
+++ TableViewApp/CustomTableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTableViewCell.swift
+//  TableViewApp
+//
+//  Created by Евгений Каратаев on 21.04.2020.
+//  Copyright © 2020 Evgen Studio. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
